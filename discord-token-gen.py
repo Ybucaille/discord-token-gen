02b62b5c83a1ebc24a5817e1657ddf5c17f787dd
shwzr <113097122+shwzr@users.noreply.github.com>
2022-12-24 14:36:30 +0100

    Update discord-token-gen.py

--- discord-token-gen.py
+++ discord-token-gen.py
@@ -1,13 +1,27 @@
 import random
-chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+import string
+import os
+from datetime import date
+import datetime
 
-for i in range(10000):
-    first = ''.join((random.choice(chars) for i in range(24)))
-    second = ''.join((random.choice(chars) for i in range(6)))
-    third = ''.join((random.choice(chars) for i in range(2)))
-    fourth = ''.join((random.choice(chars) for i in range(26)))
-    fifth = ''.join((random.choice(chars) for i in range(7)))
-    result = first + "." + second + "." + third + "__" + fourth + "_" + fifth
-    output = open("tokens.txt", "a")
-    output.write(result + "\n")
+N = input("Combien de tokens veux-tu générer ? ")
 
+count = 0
+current_path = os.path.dirname(os.path.realpath(__file__))
+
+while(int(count)<int(N)):
+    firstGen = random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+ random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+    secondGen = "MT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+    thirdGen = "NT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+    fourthGen = "MD"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+    fifthGen = "MT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+
+    f= open(current_path+"/"+str("Generated Tokens")+str("")+".txt","a")
+    f.write(firstGen+"\n"+secondGen+"\n"+thirdGen+"\n"+fourthGen+"\n"+fifthGen+"\n")
+
+    print(firstGen)
+    print(secondGen)
+    print(thirdGen)
+    print(fourthGen)
+    print(fifthGen)
+    count+=1
