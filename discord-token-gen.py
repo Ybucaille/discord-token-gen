f580c2595cf8ffca6f78364c0ef74c353c4d2f85
shwzr <113097122+shwzr@users.noreply.github.com>
2023-01-09 20:57:46 +0100

    Update discord-token-gen.py

--- discord-token-gen.py
+++ discord-token-gen.py
@@ -1,27 +1,33 @@
 import random
 import string
-import os
-from datetime import date
-import datetime
+import json
 
-N = input("Cmb de tokens veux-tu générer ? ")
+def generer_tokens(n):
+    """Génère n tokens aléatoires avec les préfixes MT, NT, et MD et les retourne sous forme de liste"""
+    tokens = [] # Liste pour stocker les tokens générés
+    prefixes = ["MT", "NT", "MD"] # Liste des préfixes disponibles
+    for _ in range(n): # Boucle pour générer n tokens
+        # Choisir un préfixe aléatoirement
+        prefix = random.choice(prefixes)
+        # Génère un token aléatoire en concaténant le préfixe et plusieurs chaînes de caractères aléatoires
+        token = prefix
+        token += random.choice(string.ascii_letters)
+        token += ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))
+        token += "."
+        token += random.choice(string.ascii_letters).upper()
+        token += ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))
+        token += "."
+        token += ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+        tokens.append(token)          
+        
+    return tokens
 
-count = 0
-current_path = os.path.dirname(os.path.realpath(__file__))
+# Demander à l'utilisateur combien de tokens il souhaite générer
+n = int(input("Combien de tokens veux-tu générer ? "))
 
-while(int(count)<int(N)):
-    firstGen = random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters).upper()+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+ random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    secondGen = "MT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    thirdGen = "NT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    fourthGen = "MD"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
-    fifthGen = "MT"+random.choice(string.ascii_letters)+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(21))+"."+random.choice(string.ascii_letters).upper()+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(5))+"."+''.join(random.choice(string.ascii_letters + string.digits) for _ in range(27))
+# Générer les tokens et afficher le résultat
+tokens = generer_tokens(n)
 
-    f= open(current_path+"/"+str("Generated Tokens")+str("")+".txt","a")
-    f.write(firstGen+"\n"+secondGen+"\n"+thirdGen+"\n"+fourthGen+"\n"+fifthGen+"\n")
-
-    print(firstGen)
-    print(secondGen)
-    print(thirdGen)
-    print(fourthGen)
-    print(fifthGen)
-    count+=1
+# Export en .json le résultat
+with open('tokens.json', 'w') as f:
+    json.dump(tokens, f)
